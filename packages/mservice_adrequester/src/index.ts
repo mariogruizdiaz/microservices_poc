177398188389efe3b954038ee6522ba671e2dd84
Morgan Guo <morgan.guo@pluto.tv>
2022-09-15 16:43:05 -0700

    refactor(adrequester): promise style clean up

--- packages/mservice_adrequester/src/index.ts
+++ packages/mservice_adrequester/src/index.ts
@@ -2,18 +2,12 @@ import 'dotenv/config';
 import Orchestrator from './servicesOrchestrator';
 
 const SERVICE_NAME = 'µ Service Ad Requester';
-async function init(): Promise<boolean> {
-    try {
-        await Orchestrator.init();
-        return Promise.resolve(true);
-    } catch (e) {
-        console.error(e);
-        return Promise.resolve(false);
-    }
-}
 
-init().then((response) => {
-    if (response)
-        console.log(`The ${SERVICE_NAME} was initialized successfully!`);
-    else console.log(`It was not possible to intialize the ${SERVICE_NAME}`);
-});
+Orchestrator.init()
+    .then(() =>
+        console.log(`The ${SERVICE_NAME} was initialized successfully!`)
+    )
+    .catch((e) => {
+        console.error(e);
+        console.log(`It was not possible to intialize the ${SERVICE_NAME}`);
+    });
--- packages/mservice_adrequester/src/services/adRequester.ts
+++ packages/mservice_adrequester/src/services/adRequester.ts
@@ -4,53 +4,64 @@ import {
     AdRequesterResponse
 } from 'enterprise_service_bus';
 
-export class AdRequester {
-    public static serviceName = 'AdRequester';
-    public static INSTANCE_ID = `adreq_${
-        process.env.INSTANCE_NAME
-    }_${Math.floor(Math.random() * 100)}`;
+const SERVICE_NAME = 'AdRequester';
+const INSTANCE_ID = `adreq_${process.env.INSTANCE_NAME}_${Math.floor(
+    Math.random() * 100
+)}`;
 
-    public static async serviceImplementation(
-        err: unknown,
-        msg: unknown
-    ): Promise<void> {
-        if (err) return Promise.reject(err);
-        const message = msg as { data: AdRequesterRequest; reply: string };
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type Last<T extends unknown[]> = T extends [...infer I, infer L] ? L : never;
 
-        console.log(
-            `The service ${
-                AdRequester.serviceName
-            } receives a new Request - PAYLOAD: ${JSON.stringify(
-                message
-            )} ****************************************`
-        );
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+type Init<T extends unknown[]> = T extends [...infer I, infer L] ? I : never;
 
-        if (message.reply) {
-            let adNumber = 0;
+type Morph<Ts extends unknown[]> = (...args: [...Init<Ts>]) => Last<Ts>;
 
-            const ads = [];
-            const total = Math.floor(Math.random() * 20);
+type Callback<E, A, R> = Morph<[E, A, R]>;
 
-            for (let i = 0; i < total; i++) {
-                ads.push(
-                    `Ad #${++adNumber} from ${
-                        AdRequester.INSTANCE_ID
-                    }.instance for the url ${message.data}`
-                );
-            }
-
-            MessagingService.response(
-                AdRequester.serviceName,
-                message.reply,
-                new AdRequesterResponse(200, ads)
-            );
-            return Promise.resolve();
-        } else {
-            return Promise.reject(
-                `Error sending request Message. Use "request" instead of "publish" for expecting a response of it. Message: ${JSON.stringify(
-                    message
-                )}`
-            );
-        }
-    }
+function withCallBackErrorHandled<E, A, R>(
+    f: Callback<E, A, R>
+): Callback<E, A, R> {
+    return (err: E, a: A) => {
+        if (err) throw err;
+        return f(err, a);
+    };
 }
+
+async function impl(_: unknown, msg: unknown): Promise<void> {
+    const message = msg as { data: AdRequesterRequest; reply: string };
+
+    console.log(
+        `The service ${SERVICE_NAME} receives a new Request - PAYLOAD: ${JSON.stringify(
+            message
+        )} ****************************************`
+    );
+
+    if (!message.reply)
+        throw `Error sending request Message. Use "request" instead of "publish" for expecting a response of it. Message: ${JSON.stringify(
+            message
+        )}`;
+    let adNumber = 0;
+
+    const ads = [];
+    const total = Math.floor(Math.random() * 20);
+
+    for (let i = 0; i < total; i++) {
+        ads.push(
+            `Ad #${++adNumber} from ${INSTANCE_ID}.instance for the url ${
+                message.data
+            }`
+        );
+    }
+
+    return MessagingService.response(
+        SERVICE_NAME,
+        message.reply,
+        new AdRequesterResponse(200, ads)
+    );
+}
+
+export const AdRequester = {
+    serviceName: SERVICE_NAME,
+    serviceImplementation: withCallBackErrorHandled(impl)
+};
